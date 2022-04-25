c06909a178c8eba42e9684afaf9319a944fe910b
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2022-04-25 22:40:07 +0200

    created a forLoop for the buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './App.css';
 
 function App() {
+  const dialBtns = () => {
+    const numbers = [];
+
+    for (let i = 1; i < 10; i++) {
+      numbers.push(<button key={i}>{i}</button>);
+    }
+    return numbers;
+  };
   return (
-    
+    <main>
+      <section className='dialer'>
+        <div className='display'></div>
+        <div className='digits'>
+          {dialBtns()}
+          <button>0</button>
+          <button>*</button>
+          <button>#</button>
+        </div>
+        <div>
+          <button>=</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
