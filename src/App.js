405457ee2a99fcc838220b24c65803fba19af738
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2022-04-26 10:45:54 +0200

    started to sum all the digits

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [digit, setDigit] = useState('');
+  const [sum, setSum] = useState('');
+
+  const updateDisplay = (value) => {
+    setDigit(digit + value);
+  };
+
   const dialBtns = () => {
     const numbers = [];
 
     for (let i = 1; i < 10; i++) {
-      numbers.push(<button key={i}>{i}</button>);
+      numbers.push(
+        <button onClick={() => updateDisplay(i.toString())} key={i}>
+          {i}
+        </button>
+      );
     }
     return numbers;
   };
+
+  let value = document.getElementById('output').value,
+    total = sum;
+
+  const sumDigits = () => {
+    while (value) {
+      total += value % 10;
+      value = Math.floor(value / 10);
+    }
+    let result = document.getElementById('output');
+    result.value = total;
+  };
+
+  const calculate = () => {
+    setSum(sumDigits(total).toString());
+  };
+
   return (
     <main>
       <section className='dialer'>
-        <div className='display'></div>
+        <div className='display'>
+          <form>
+            <input type='text' value={digit} id='output' readOnly></input>
+          </form>
+        </div>
+        <div className='operator'>
+          <button onClick={() => updateDisplay({ calculate })}>
+            SUM IT UP
+          </button>
+          <button onClick={() => updateDisplay('0')}>CLEAR</button>
+        </div>
         <div className='digits'>
           {dialBtns()}
-          <button>0</button>
-          <button>*</button>
-          <button>#</button>
-        </div>
-        <div>
-          <button>=</button>
+          <button onClick={() => updateDisplay('*')}>*</button>
+          <button onClick={() => updateDisplay('0')}>0</button>
+          <button onClick={() => updateDisplay('#')}>#</button>
         </div>
       </section>
     </main>
